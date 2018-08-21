494a00219ef230b1e389f4de603f6c32b59d7567
Zeah <arild@effektivmarkedsforing.no>
2018-08-21 16:06:36 +0200

    55

---
+++ em-lanlist.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: EM Lånlist 
+Description: liste over Lån
+Version: 0.0.1
+GitHub Plugin URI: zeah/EM-lanlist
+*/
+
+defined('ABSPATH') or die('Blank Space');
+
+// constant for plugin location
+define('LANLIST_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+require_once 'inc/lan-posttype.php';
+require_once 'inc/lan-shortcode.php';
+
+function init_emlanlist() {
+	Lan_posttype::get_instance();
+	Lan_shortcode::get_instance();
+}
+add_action('plugins_loaded', 'init_emlanlist');
